0c353cf52473fef715702b1e7640c3c1a49dce81
Yangx <641160771@qq.com>
2025-01-21 21:30:47 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import InteractiveAvatar from "@/components/InteractiveAvatar";
 export default function App() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex flex-col">
       <div className="flex flex-col items-start justify-start gap-5 mx-auto pt-5 pb-5">
         <div className="w-full">
           <InteractiveAvatar />
